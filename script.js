8073b1290614babc82c1b621d5c19021093b2f6c
YesipovAndrii <esipov1995@gmail.com>
2025-01-30 03:05:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const imageContainer = document.querySelector('.image-container');
-    const homeLink = document.createElement('a');
-    homeLink.href = 'index.html';
-    imageContainer.appendChild(homeLink);
+    const carouselImages = document.querySelectorAll('.carousel img');
+    let currentIndex = 0;
 
-    window.addEventListener('scroll', function() {
-        if (window.scrollY > 0) {
-            imageContainer.classList.add('scroll');
-            document.querySelector('footer').classList.add('sticky-footer');
-        } else {
-            imageContainer.classList.remove('scroll');
-            document.querySelector('footer').classList.remove('sticky-footer');
-        }
-    });
+    function showNextImage() {
+        carouselImages[currentIndex].classList.remove('active');
+        currentIndex = (currentIndex + 1) % carouselImages.length;
+        carouselImages[currentIndex].classList.add('active');
+    }
 
-    homeLink.addEventListener('click', function(event) {
-        event.preventDefault();
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
+    setInterval(showNextImage, 5000); // Смена изображений каждые 5 секунд
 });

d923a5235ed75aed57bb401e621cf31c7fc88aff
YesipovAndrii <esipov1995@gmail.com>
2025-01-30 11:51:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const carouselImages = document.querySelectorAll('.carousel img');
-    let currentIndex = 0;
+    
+    if (carouselImages.length > 0) { // Проверяем наличие карусели
+        let currentIndex = 0;
 
-    function showNextImage() {
-        carouselImages[currentIndex].classList.remove('active');
-        currentIndex = (currentIndex + 1) % carouselImages.length;
-        carouselImages[currentIndex].classList.add('active');
+        function showNextImage() {
+            carouselImages[currentIndex].classList.remove('active');
+            currentIndex = (currentIndex + 1) % carouselImages.length;
+            carouselImages[currentIndex].classList.add('active');
+        }
+
+        setInterval(showNextImage, 5000); // Смена изображений каждые 5 секунд
     }
 
-    setInterval(showNextImage, 50); // Смена изображений каждые 5 секунд
+    const navbar = document.querySelector('.navbar');
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 0) {
+            navbar.classList.add('sticky-navbar');
+        } else {
+            navbar.classList.remove('sticky-navbar');
+        }
+    });
 });
